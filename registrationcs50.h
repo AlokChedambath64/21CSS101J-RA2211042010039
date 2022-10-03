e2d037f6923419a35fb4407cf4aa6a374e615033
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-04 00:16:03 +0530

    Create registrationcs50.h
    Program that prompts user for their registration number using the get_string function. Then prints "This is your registration number: (user input)".

---
+++ registrationcs50.h
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+  string registration= get_string("Registration Number: ");
+  printf("This is your Registration Number:%s\n", registration);
+  return 0;
+}
+    
