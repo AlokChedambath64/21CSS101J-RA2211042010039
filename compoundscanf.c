bc36f842af1c09f36888b757cfe4fb08fd855526
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-03 23:55:41 +0530

    Create compoundscanf.c
    Program to find out compound interest using the scanf function.

---
+++ compoundscanf.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float principal, rate, time, CI;
+	//input principal
+	printf("Enter Principal Amount: ");
+	scanf("%f", &principal);
+	//input rate
+	printf("Enter Rate: ");
+	scanf("%f", &rate);
+	//input time
+	printf("Enter Time: ");
+	scanf("%f", &time);
+	
+	//Calculation of compound Interest
+	CI= principal*(pow((1+rate/100), time));
+	printf("Final Amount: %f", CI);
+	
+	return 0;
+}
+		
