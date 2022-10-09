59fac294b648aca770484bbe64c11f835253b21a
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 22:45:47 +0530

    Create parallogramperimeter.c

---
+++ parallogramperimeter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+  float perimeter, length, base;
+  printf("Base: ");
+  scanf("%f", &base);
+  printf("Length:");
+  scanf("%f", &length);
+         
+  printf("%f", perimeter = 2*(length*base));
+}
