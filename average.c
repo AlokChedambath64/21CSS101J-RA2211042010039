7741019246c02164ff94244efd3ac37445b4c22d
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 18:56:32 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int n1, n2, n3;
+  printf("Number 1: ");
+  scanf("%d", &n1);
+  
+  printf("Number 2: ");
+  scanf("%d", &n2);
+  
+  printf("Number 3: ");
+  scanf("%d", &n3);
+  
+  printf("Average: %f\n", (n1+n2+n3)/3.0);
+}
+  
