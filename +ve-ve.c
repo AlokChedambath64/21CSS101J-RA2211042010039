798afc8265616df74c14e5071a52ead924e39c46
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 15:46:23 +0530

    Create +ve-ve.c

---
+++ +ve-ve.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+ {
+  int n;
+  printf("Please type in your number: ");
+  scanf("%d", &n);
+  
+  if (n>0)
+  {
+  printf("The number is positive\n");
+  }
+  else if (n<0)
+  {
+  printf("The number is negitive\n");
+  }
+ }
