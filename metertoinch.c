a4ff5082b9000c347e0555a10a8667a4b0ea3323
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 21:19:33 +0530

    Create metertoinch.c

---
+++ metertoinch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  float meter, inch;
+  printf("Enter the distance in meter");
+  scanf("%f", &meter);
+  inch= meter/39.37;
+  printf("%f", inch);
+}
+         
