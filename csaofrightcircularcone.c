3832c52eeda8637fbfbc9f971908b25c32511025
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 13:34:54 +0530

    Create csaofrightcircularcone.c

---
+++ csaofrightcircularcone.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int radius, length, CSA;
+  printf("Radius: ");
+  scanf("%d", &radius);
+  printf("Length: ");
+  scanf("%d", &length);
+  
+  CSA = 3.14*radius*length;
+  printf("%d", CSA);
+}
+  
+  
