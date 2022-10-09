468e0023f2034421c364ff49a534bee7ce4a7d25
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 16:03:02 +0530

    Create circumference.c

---
+++ circumference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  int radius, circumference;
+  printf("Radius: ");
+  scanf("%d", &radius);
+  
+  circumference = 2*3.14*radius;
+  printf("%d", circumference);
+}
+         
+   
