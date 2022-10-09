a9c30003ef49f557b44ea2e981575e7b9d9b7479
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-09 22:00:05 +0530

    Create celciustofarenheit.c

---
+++ celciustofarenheit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  float farenheit, celcius;
+  printf("Enter the temperature in celcius: ");
+  scanf("%f", &celcius);
+  farenheit=((celcius*1.8)+32;
+  printf("temperature is :%f", farenheit);
+  return 0;
+}
