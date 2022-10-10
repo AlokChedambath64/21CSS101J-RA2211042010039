1ce89d80e3615796b99ca50bb88fa9256ff9edc0
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-10 20:41:55 +0530

    Create perimeteroftrapezium.c

---
+++ perimeteroftrapezium.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+int side1, side2, side3, side4, peri;
+printf("Side1, Side2, Side3, Side 4");
+scanf("%d%d%d%d", side1, side2, side3, side4);
+
+printf("perimeter=%d", peri= side1+side2+side3+side4);
+}
