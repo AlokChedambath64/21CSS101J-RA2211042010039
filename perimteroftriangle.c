4d3164f392d372f09249b8b597f0a0a9052dad17
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-14 13:38:44 +0530

    Update perimteroftriangle.c

--- perimteroftriangle.c
+++ perimteroftriangle.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+  printf("Registration Number: RA2211042010039\n");
   int side1, side2, side3, perimeter;
   printf("Side-1: , Side-2:, Side-3: ");
   scanf("%d%d%d", &side1, &side2, &side3);
