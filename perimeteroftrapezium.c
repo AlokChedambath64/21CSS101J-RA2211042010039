931cee261ab07eed7ca71ec55992d12d04be1d8b
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-14 13:38:03 +0530

    Update perimeteroftrapezium.c

--- perimeteroftrapezium.c
+++ perimeteroftrapezium.c
@@ -2,9 +2,10 @@
 
 int main()
 {
-int side1, side2, side3, side4, peri;
-printf("Side1, Side2, Side3, Side 4");
-scanf("%d%d%d%d", side1, side2, side3, side4);
+  printf("Registration Number: RA2211042010039\n");
+  int side1, side2, side3, side4, peri;
+  printf("Side1, Side2, Side3, Side 4");
+  scanf("%d%d%d%d", side1, side2, side3, side4);
 
-printf("perimeter=%d", peri= side1+side2+side3+side4);
+  printf("perimeter=%d", peri= side1+side2+side3+side4);
 }
