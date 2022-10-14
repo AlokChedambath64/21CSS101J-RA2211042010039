4cebb02d7e3c45efe72b9dd88c83a4ad6443fee0
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-10-14 13:45:08 +0530

    Update simple.c

--- simple.c
+++ simple.c
@@ -2,6 +2,7 @@
 
  int main()
 {
+    printf("Registration Number: RA2211042010039\n");
     int principal,interest,year,result;
     scanf("(%d,%d,%d)", &principal, &interest, &year);
     result=(principal*interest*year)/100;
