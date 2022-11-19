b87812f7136454716ec187cb8cc8be9dd0864e5d
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-11-19 17:46:36 +0530

    Create ifelsedemo2.c

---
+++ ifelsedemo2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main();
+{
+  printf("RA2211042010039\n");
+  float balance;
+  printf("Enter Balance : ");
+  scanf("%f", &balance);
+  if (balance > 1000);
+  {
+    printf("Sufficient balance\n");
+  }
+  else 
+  {
+    printf("Balance is low\n");
+  }
+}
