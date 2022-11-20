235b34972106b278b4e6f5a2a2744a39d9c506b2
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-11-21 00:52:01 +0530

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("RA2211042010039\n");
+    float floattype;
+    double doubletype;
+    printf("Size of int: %zu bytes\n", sizeof(100));
+    printf("Size of float: %zu bytes\n", sizeof(floattype));
+    printf("Size of double: %zu bytes\n", sizeof(doubletype));
+    printf("Size of char: %zu bytes\n", sizeof(char));
+}
