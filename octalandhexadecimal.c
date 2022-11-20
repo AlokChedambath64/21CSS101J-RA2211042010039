56ae492896f68ddd8285300d3974ef0e0b7e377f
AlokChedambath64 <110228030+AlokChedambath64@users.noreply.github.com>
2022-11-21 00:48:37 +0530

    Create octalandhexadecimal.c

---
+++ octalandhexadecimal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    printf("RA2211042010039\n");
+    float d;
+    int o,x;
+    printf("Enter a decimal integer : ");
+    scanf("%f", &d);
+    o=d;
+    printf("Octal number : %o\n", o);
+    x=d;
+    printf("Hexadecimal number : %x\n", x);
+}
